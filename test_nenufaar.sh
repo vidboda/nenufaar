18443c9db0ac7786099d049a0a8e253ca3b17541
dadoule <david.baux@inserm.fr>
2017-03-24 16:16:39 +0100

    modified ath for log files

--- test_nenufaar.sh
+++ test_nenufaar.sh
@@ -32,28 +32,28 @@ case "${KEY}" in
 esac
 shift
 done
-mkdir test_logs/${VERSION}/
-touch test_logs/${VERSION}/SUMMARY.log
-sh nenufaar.sh -i input/MiniFastqTest/ -hsm true -up false -p wgs -log test_logs/${VERSION}/${VERSION}.wgs.log -cu false
+mkdir test_logs/nenufaar/${VERSION}/
+touch test_logs/nenufaar/${VERSION}/SUMMARY.log
+sh nenufaar.sh -i input/MiniFastqTest/ -hsm true -up false -p wgs -log test_logs/nenufaar/${VERSION}/${VERSION}.wgs.log -cu false
 STATUS=$?
 if [ "${STATUS}" -eq 0 ];then
-	echo "Test WGS OK on ${HOSTNAME}" > test_logs/${VERSION}/SUMMARY.log
+	echo "Test WGS OK on ${HOSTNAME}" > test_logs/nenufaar/${VERSION}/SUMMARY.log
 else
-	echo "Test WGS NOT OK on ${HOSTNAME} - check ${VERSION}.wgs.log" > test_logs/${VERSION}/SUMMARY.log
+	echo "Test WGS NOT OK on ${HOSTNAME} - check ${VERSION}.wgs.log" > test_logs/nenufaar/${VERSION}/SUMMARY.log
 fi
-sh nenufaar.sh -i input/MiniFastqTest/ -hsm true -up false -p capture -log test_logs/${VERSION}/${VERSION}.capture.log -cu false -a annovar -l gene_lists/ns.txt
+sh nenufaar.sh -i input/MiniFastqTest/ -hsm true -up false -p capture -log test_logs/nenufaar/${VERSION}/${VERSION}.capture.log -cu false -a annovar -l gene_lists/ns.txt
 STATUS=$?
 if [ "${STATUS}" -eq 0 ];then
-	echo "Test capture OK on ${HOSTNAME}" >> test_logs/${VERSION}/SUMMARY.log
+	echo "Test capture OK on ${HOSTNAME}" >> test_logs/nenufaar/${VERSION}/SUMMARY.log
 else
-	echo "Test capture NOT OK on ${HOSTNAME} - check ${VERSION}.capture.log" >> test_logs/${VERSION}/SUMMARY.log
+	echo "Test capture NOT OK on ${HOSTNAME} - check ${VERSION}.capture.log" >> test_logs/nenufaar/${VERSION}/SUMMARY.log
 fi
-sh nenufaar.sh -i input/MiniFastqTest/ -hsm true -up false -p amplicon -c ug -log test_logs/${VERSION}/${VERSION}.amplicon.log
+sh nenufaar.sh -i input/MiniFastqTest/ -hsm true -up false -p amplicon -c ug -log test_logs/nenufaar/${VERSION}/${VERSION}.amplicon.log
 STATUS=$?
 if [ "${STATUS}" -eq 0 ];then
-	echo "Test amplicon OK on ${HOSTNAME}" >> test_logs/${VERSION}/SUMMARY.log
+	echo "Test amplicon OK on ${HOSTNAME}" >> test_logs/nenufaar/${VERSION}/SUMMARY.log
 else
-	echo "Test amplicon NOT OK on ${HOSTNAME} - check ${VERSION}.capture.log" >> test_logs/${VERSION}/SUMMARY.log
+	echo "Test amplicon NOT OK on ${HOSTNAME} - check ${VERSION}.capture.log" >> test_logs/nenufaar/${VERSION}/SUMMARY.log
 fi
 
 exit
