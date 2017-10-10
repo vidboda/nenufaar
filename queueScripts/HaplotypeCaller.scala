26891888d3ee325b915698c123bf9186b0f9c96c
beboche <david.baux@inserm.fr>
2017-10-10 17:57:44 +0200

    modified scala script to pass scattercount as an arg

--- queueScripts/HaplotypeCaller.scala
+++ queueScripts/HaplotypeCaller.scala
@@ -20,23 +20,26 @@ class performHaplotypeCaller extends QScript {
 	@Output(doc="Output vcf file", shortName="o")
 	var outputFile: File = _
 
-	@Argument(doc="The minimum phred-scaled confidence threshold at which variants should be called", shortName="stand_emit_conf")
-        var standEmitConf: Int = _
+	//@Argument(doc="The minimum phred-scaled confidence threshold at which variants should be called", shortName="stand_emit_conf")
+	//var standEmitConf: Int = _
 
 	@Argument(doc="The minimum phred-scaled confidence threshold at which variants should be emitted (and filtered with LowQual if less than the calling threshold)", shortName="stand_call_conf")
-        var standCallConf: Int = _
+    var standCallConf: Int = _
 
 	@Argument(doc="List of Filters", shortName="A")
 	var annot: List[String] = Nil
+	
+	@Argument(doc="Number of available cores to define scatterCount", shortName="nbThreads")
+    var nbThreads: Int = _
 
 	def script() {
 		val haplotypeCaller = new HaplotypeCaller
-		haplotypeCaller.scatterCount = 28
+		haplotypeCaller.scatterCount = nbThreads
 		haplotypeCaller.reference_sequence = referenceFile
 		haplotypeCaller.input_file :+= bamFile
 		haplotypeCaller.intervals = if (performHaplotypeCaller.this.intervals == null) Nil else List(performHaplotypeCaller.this.intervals)
 		haplotypeCaller.dbsnp = snpFile
-		haplotypeCaller.stand_emit_conf = standEmitConf
+		//haplotypeCaller.stand_emit_conf = standEmitConf - removed in GATK 3.8
 		haplotypeCaller.stand_call_conf = standCallConf
 		haplotypeCaller.genotyping_mode = GenotypingOutputMode.DISCOVERY
 		haplotypeCaller.maxAltAlleles = 10
